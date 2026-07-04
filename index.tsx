a43a375ac15b768110601c604b9ca4218bbad269
Chad Jalban <chadjalban@gmail.com>
2026-07-04 22:32:14 +0800

    Update patches

--- index.tsx
+++ index.tsx
@@ -21,7 +21,7 @@ export default definePlugin({
 
     patches: [
         {
-            find: ".DISPLAY_NAME_STYLES_COACHMARK)",
+            find: "#{intl::USER_PROFILE_ACCOUNT_POPOUT_BUTTON_A11Y_LABEL}",
             replacement: {
                 // react.jsx)(AccountPanel or $self.PanelWrapper, { ..., showTaglessAccountPanel: blah })
                 match: /(?<=\i\.jsxs?\)\()(\i(?:\.\i)?),{(?=[^}]*?userTag:\i,occluded:)/,
